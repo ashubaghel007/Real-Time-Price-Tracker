849726ae128bdda95ac16ba4db3ff07b8b8ca621
ashubaghel007 <ashubaghel007@gmail.com>
2025-11-25 23:48:56 +0400

    added Initial project

---
+++ RealTimePriceTracker/RealTimePriceTracker/RealTimePriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimePriceTrackerApp.swift
+//  RealTimePriceTracker
+//
+//  Created by Ashish Baghel on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct RealTimePriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
