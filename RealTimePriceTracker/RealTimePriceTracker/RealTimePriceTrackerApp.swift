30d2997fd641c2a71feb60d0c0c95ddf7b27ba74
ashubaghel007 <ashubaghel007@gmail.com>
2025-11-26 02:51:50 +0400

    Removed unused ContentView

--- RealTimePriceTracker/RealTimePriceTracker/RealTimePriceTrackerApp.swift
+++ RealTimePriceTracker/RealTimePriceTracker/RealTimePriceTrackerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RealTimePriceTrackerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RTPTStocksFeedView()
         }
     }
 }
